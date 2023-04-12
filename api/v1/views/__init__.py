14b70f6fa290978d50f161378f62d45928a3dd68
simonrichard-dev <simon.insa@gmail.com>
2023-04-12 15:36:32 +0200

    fix models import and Blueprint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,8 @@
 Blueprint of the API
 """
 
-from flask import blueprints
-app_views = blueprints('app_views', __name__, url_prefixe='/api/v1')
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefixe='/api/v1')
 
 from api.v1.views.index import *
