3a7acba34b718cdfacb4e5e46baab9d8ec723962
simonrichard-dev <simon.insa@gmail.com>
2023-04-12 15:48:38 +0200

    fix prefix orth

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,13 @@ starts the API
 """
 
 from flask import Flask
-from models import storage
 from api.v1.views import app_views
 from os import getenv
+from models import storage
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefixe='/api/v1')
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
