733595c820d2749a37e5fe759c350b21d99a22d9
bapt2 <5711@holbertonstudents.com>
2023-04-12 14:41:27 +0200

    adding some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,4 +17,13 @@ from models.user import User
 
 @app_views.route('/status')
 def status():
-    return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats')
+def stats():
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)
+                    })
