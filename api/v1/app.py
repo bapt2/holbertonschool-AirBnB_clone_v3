17e6cc4178ecc2338f3c21a9f333e1dd3ffe331d
bapt2 <5711@holbertonstudents.com>
2023-04-14 08:28:50 +0200

    adding HTTP acces control

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,12 @@ from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 from os import getenv
 from models import storage
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
-
+cors = CORS(app, resources={r'/*': {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
